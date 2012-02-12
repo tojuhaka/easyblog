54f964b3d261bdcf7590dc73c3d554ebb2da40d6
tojuhaka <tojuhaka@gmail.com>
2012-02-13 01:21:43 +0200

    pagination added TODO: make it work

--- easyblog/static/js/coffee/saapas.coffee
+++ easyblog/static/js/coffee/saapas.coffee
@@ -1,3 +1,10 @@
-$('#myCarousel').carousel
+jQuery ($) ->
+    $('#myCarousel').carousel
       interval: 8000
 
+    $('#items').pajinate ->
+          items_per_page: 1
+          nav_label_first: '<<'
+          nav_label_last: '>>'
+          nav_label_prev: '<'
+          nav_label_next: '>'
--- easyblog/static/js/saapas.js
+++ easyblog/static/js/saapas.js
@@ -1,7 +1,18 @@
 (function() {
 
-  $('#myCarousel').carousel({
-    interval: 8000
+  jQuery(function($) {
+    $('#myCarousel').carousel({
+      interval: 8000
+    });
+    return $('#items').pajinate(function() {
+      return {
+        items_per_page: 1,
+        nav_label_first: '<<',
+        nav_label_last: '>>',
+        nav_label_prev: '<',
+        nav_label_next: '>'
+      };
+    });
   });
 
 }).call(this);
