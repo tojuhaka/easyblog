34b9d927794955929b924d9fb4f78b4644b1a10f
tojuhaka <tojuhaka@gmail.com>
2012-03-19 10:17:28 +0200

    translation updated

--- easyblog/config.py
+++ easyblog/config.py
@@ -1,16 +1,18 @@
 from easyblog import _
 msg = {
-    'content_forbidden':  u"You have no permission to review this content",
+    'content_forbidden':  _(msgid="You have no permission to review this content",
+default=u"You have no permission to review this content"),
+
     'password_invalid':  _(msgid="password_invalid", default=u"Password is invalid."),
     'logged_in_as': _(msgid=u"logged_in_as", 
             default="You are logged in as "),
-    'succeed_add_user': u"Successfully added user",
-    'succeed_news_create': u"Successfully created news item",
+    'succeed_add_user': _(msgid=u"Successfully added user", default=u"Successfully added user"),
+    'succeed_news_create': _(msgid=u"Successfully created news item", default="Successfully created news item"),
     'saved': _(msgid=u"saved", default="Changes successfully saved"),
-    'no_results': u'No search results',
-    'items_removed': u'Items successfully removed',
-    'create_post': u"Create a single post under the blog by defining it's \
-            title and content. The posts are shown under the blogs description",
+    'no_results': _(msgid=u'No search results', default="No search results"),
+    'items_removed': _(msgid=u'Items successfully removed', default="Items successfully removed"),
+    'create_post': _(msgid=u"Create a single post under the blog by defining it's title and content. The posts are shown under the blogs description",
+            default="Create a single post under the blog by defining it's title and content. The posts are shonw under the blog description"),
     'nothing_changed': _(msgid=u"nothing_changed", default="Nothing Changed"),
 
     }
